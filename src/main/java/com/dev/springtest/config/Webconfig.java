f74b247e9e7952dba6137b18563a39ded0c6b74d
karsv <Pbvf2019!>
2020-02-14 18:09:08 +0200

    checkstyle

--- src/main/java/com/dev/springtest/config/Webconfig.java
+++ src/main/java/com/dev/springtest/config/Webconfig.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.view.JstlView;
 public class Webconfig {
 
     @Bean
-    public InternalResourceViewResolver resolver(){
+    public InternalResourceViewResolver resolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setViewClass(JstlView.class);
         resolver.setPrefix("/WEB-INF/views/");
